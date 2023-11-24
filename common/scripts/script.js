5747fe228fc5dc14414dce3dd7fae4b9565808af
Disspear574 <disspear574@mail.ru>
2023-11-24 21:10:36 +0500

    modified:   common/scripts/script.js

--- common/scripts/script.js
+++ common/scripts/script.js
@@ -4,18 +4,37 @@ $(document).ready(function () {
 		$('.header').toggleClass('header-shrink', $(this).scrollTop() > 50)
 	})
 })
+var w = window.innerWidth ? window.innerWidth : $(window).width()
 
-function reveal() {
-	var reveals = document.querySelectorAll('.reveal')
-	for (var i = 0; i < reveals.length; i++) {
-		var windowHeight = window.innerHeight
-		var elementTop = reveals[i].getBoundingClientRect().top
-		var elementVisible = 150
-		if (elementTop < windowHeight - elementVisible) {
-			reveals[i].classList.add('active')
-		} else {
-			reveals[i].classList.remove('active')
+//Если ширина меньше 640px
+if (w <= 640) {
+	function reveal() {
+		var reveals = document.querySelectorAll('.reveal')
+		for (var i = 0; i < reveals.length; i++) {
+			var windowHeight = window.innerHeight
+			var elementTop = reveals[i].getBoundingClientRect().top
+			var elementVisible = -400
+			if (elementTop < windowHeight - elementVisible) {
+				reveals[i].classList.add('active')
+			} else {
+				reveals[i].classList.remove('active')
+			}
 		}
 	}
+	window.addEventListener('scroll', reveal)
+} else {
+	function reveal() {
+		var reveals = document.querySelectorAll('.reveal')
+		for (var i = 0; i < reveals.length; i++) {
+			var windowHeight = window.innerHeight
+			var elementTop = reveals[i].getBoundingClientRect().top
+			var elementVisible = 150
+			if (elementTop < windowHeight - elementVisible) {
+				reveals[i].classList.add('active')
+			} else {
+				reveals[i].classList.remove('active')
+			}
+		}
+	}
+	window.addEventListener('scroll', reveal)
 }
-window.addEventListener('scroll', reveal)
